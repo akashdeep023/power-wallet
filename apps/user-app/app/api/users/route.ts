5c6a74c62b49e50887bdff046d15af50f3eb602f
akashdeep023 <ad3500476@gmail.com>
2025-09-04 03:43:53 +0530

    feat: create user API route with Prisma integration

---
+++ apps/user-app/app/api/users/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@repo/db";
+
+export const GET = async () => {
+	await prisma.user.create({
+		data: {
+			email: "asd",
+			name: "adsads",
+		},
+	});
+	return NextResponse.json({
+		message: "hi there",
+	});
+};
